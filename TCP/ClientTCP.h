9929e93b026b53487918a085b4d533ac3f859eab
AlecShunnarahSC <alec@saturnergonomics.com>
2018-09-11 17:54:49 -0500

    Organized files into UDP and TCP folders for the project and created header files for C classes

---
+++ TCP/ClientTCP.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+
+#define PORT "10022" // the port client will be connecting to 
+
+#define MAXDATASIZE 100 // max number of bytes we can get at once 
+
---
+++ UDP/ServerUDP.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define MYPORT "10022"	// the port users will be connecting to
+
+#define MAXBUFLEN 100
