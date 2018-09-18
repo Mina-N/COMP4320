fc879cf3a16a06a4f6d15dca2d43ce3c2a5be72b
AlecShunnarahSC <alec@saturnergonomics.com>
2018-09-18 12:44:46 -0500

    Added changes to UDP Server

--- UDP/ServerUDP.h
+++ UDP/ServerUDP.h
@@ -13,13 +13,52 @@
 
 #define MAXBUFLEN 100
 
+struct message_send
+{
+  u_int8_t total_message_length;
+  u_int8_t request_ID;
+  u_int8_t error_code;
+  int32_t result;
+} __attribute__((__packed__));
 
-typedef struct message_response {
-    u_int8_t total_message_length:8;
-    u_int8_t request_ID:8;
-    u_int8_t error_code:8;
-    int32_t result:32;
+struct message_receive
+{
+  size_t total_message_length;
+  unsigned int request_id;
+  unsigned int op_code;
+  unsigned int num_operands;
+  signed int op_1;
+  signed int op_2;
+} __attribute__((__packed__));
 
-} message_response;
+enum opCodes{addOp, subOp, orOp, andOp, shRightOp, shLeftOp, notOp};
 
-enum opCodes{add, sub, or, and, shRight, shLeft, not};
+// calculates the result to be sent back depending on the opCode
+void getResult(int op1, int opCode, int op2, int result);
+
+// sets the op2 value if it's not a NOT operation and sets error_code
+// to see it meets total message length requirements and op code requirements
+void checkErrors(struct message_receive *buf, int op2, int error_code);
+
+// adds two operands together
+void add(int op1, int op2, int result);
+
+// subtracts two operands 
+void subtract(int op1, int op2, int result);
+
+// performs bitwise OR on two operands
+void OR(int op1, int op2, int result);
+
+// performs bitwise AND on two operands
+void AND(int op1, int op2, int result);
+
+// performs right logical shift on two operands
+void rShift(int op1, int op2, int result);
+
+// performs left logical shift on two operands
+void lShift(int op1, int op2, int result);
+
+// performs bitwise NOT on two operands
+void NOT(int op, int result);
+
+
