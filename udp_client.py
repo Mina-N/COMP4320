32aaf1e8241d66ec4edad3c7b206e969869f51e2
root <root@DESKTOP-GA99JOE.localdomain>
2018-09-20 10:02:00 -0500

    changed file to calculate time in nano seconds and few other bug fixes

--- udp_client.py
+++ udp_client.py
@@ -62,19 +62,19 @@ while(send_message != 2):
     op_1 = 16
     op_2 = 16
 
-    opCode = eval(input("Please enter an Opcode between 0 and 6: "))
+    opCode = eval(input("Please enter an Opcode between 0 and 6 to start: "))
     while (opCode < 0 or opCode > 6):
         opCode = eval(input("Invalid Entry: Please enter an Opcode between 0 and 6: "))
 
     op1 = eval(input("Please enter operand 1: "))
 
+    #total_message_length = 8
+
     if (opCode == 6):
-        total_message_length = 6
         num_operands = 1
         op2 = 0
 
     else:
-        total_message_length = 8
         num_operands = 2
         op2 = eval(input("Please enter operand 2: "))
 
@@ -89,11 +89,12 @@ while(send_message != 2):
     #Pack the message into a Struct in Big Endian
     message = pack('!bbbbhh', total_message_length, request_id , op_code, num_operands, op_1, op_2)
 
+
     #Send a message and measure time it takes to send a message
-    send_start = time.time()
+    send_start = time.time_ns()
     send = s.sendto(message, (serverName, portNumber))
-    send_time = time.time()- send_start
-    print ("Time to send: {}".format(send_time))
+    send_time = time.time_ns()- send_start
+    print ("Time to send in nano seconds: {}".format(send_time))
 
     #Check to see if you can send
     if(send <= 0):
@@ -104,23 +105,25 @@ while(send_message != 2):
     print ("Message being sent is: {}".format(message))
 
     #Make sure valid msg, recv the message and measure time it takes to recv a message
-    recv_start = time.time()
+    recv_start = time.time_ns()
     try:
         numBytes = s.recv(max_data_size)
     except socket.error:
-        sys.stderr.write("Error: Recieved\n")
+        sys.stderr.write("Error: Received\n")
         sys.exit()
 
-    recv_time = time.time()- recv_start
-    print ("Time to recv: {}".format(recv_time))
+    recv_time = time.time_ns()- recv_start
+    rount_trip_time = recv_time - send_time
+    print ("Time to recv in nano seconds: {}".format(recv_time))
 
     print ("Message being received is: {}".format(numBytes))
 
+    round_trip_time = send_time - recv_time
+    print ("Total Round Trip Time in nano seconds is: {}".format(abs(round_trip_time)))
+
     #not sure about this
     if (numBytes == ''):
-        sys.stderr.write("recv\n")
+        sys.stderr.write("nothing received\n")
 
     request_id += 1
-    s.close()
-
-exit(1)
+s.close()
