7c497fe90847ebbe22cbb5a1fc980ef41de7fc2f
Mina Narayanan <minanarayanan@Minas-MacBook-Pro.local>
2018-10-21 20:43:26 -0500

    added while loop

--- Lab2/Slave.py
+++ Lab2/Slave.py
@@ -3,6 +3,7 @@ import struct
 import sys
 
 magicNumber = 0x4A6F7921
+addAnotherSlave = 1
 
 if (len(sys.argv) != 3):
     print("usage: slave masterhostname portnumber")
@@ -17,27 +18,29 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(server_address)
 
 try:
-    # prompt for GID
-    gid_slave = input("Please enter GID: ")
-    message_repack = struct.pack('>bl', gid_slave, magicNumber) #gid is one byte, magicNumber is four bytes
-    print >>sys.stderr, 'sending slave message to master'
-    sock.send(message_repack)
+    while(addAnotherSlave = 1):
+        # prompt for GID
+        gid_slave = input("Please enter GID: ")
+        message_repack = struct.pack('>bl', gid_slave, magicNumber) #gid is one byte, magicNumber is four bytes
+        print >>sys.stderr, 'sending slave message to master'
+        sock.send(message_repack)
 
-    # Look for the response
-    longest_message_size = 100 #TODO: CHANGE
-    print("Waiting for server (master) message.\n")
-    message = sock.recv(longest_message_size)
-    if not message:
-        print("No message received. Closing socket.\n")
-        sock.close()
-    message_unpacked = struct.unpack('>blbl', message)
-    gid_master = message[0]
-    magicNumberMaster = message[1]
-    slaveRID = message[2]
-    nextSlaveIP = message[3]
-    print("GID of master: " + str(gid_master) + "\n")
-    print("My RID: " + str(slaveRID) + "\n")
-    print("IP Address of Next Slave: " + str(nextSlaveIP) + "\n")
+        # Look for the response
+        longest_message_size = 100 #TODO: CHANGE
+        print("Waiting for server (master) message.\n")
+        message = sock.recv(longest_message_size)
+        if not message:
+            print("No message received. Closing socket.\n")
+            sock.close()
+        message_unpacked = struct.unpack('>blbl', message)
+        gid_master = message[0]
+        magicNumberMaster = message[1]
+        slaveRID = message[2]
+        nextSlaveIP = message[3]
+        print("GID of master: " + str(gid_master) + "\n")
+        print("My RID: " + str(slaveRID) + "\n")
+        print("IP Address of Next Slave: " + str(nextSlaveIP) + "\n")
+        addAnotherSlave = input("Press 1 to add another slave, or 0 to exit. ")
 
 finally:
     print >>sys.stderr, 'closing socket'
