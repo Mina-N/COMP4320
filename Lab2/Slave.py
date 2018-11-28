415fcef4c5dc9dc3b833e8b4a4ccc8ad0c86d26e
Amir Hashemian <amir7779@gmail.com>
2018-11-28 16:46:02 -0600

    master.c
    implemented select()

--- Lab2/Slave.py
+++ Lab2/Slave.py
@@ -22,8 +22,7 @@ server_address = (master_hostname, listening_port)
 
 
 try:
-    INPUT = input("Press 1 to add a Slave node:\nPress 2 to send a message:\nPress 3 to exit:\n")
-    while(INPUT == 1):
+    while(addAnotherSlave == 1):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect(server_address)
         # prompt for GID
@@ -46,27 +45,12 @@ try:
         slaveRID = message_unpacked[2]
         nextSlaveIP = hex(message_unpacked[3])
 	nextSlaveIP = nextSlaveIP[2:]
-        print("GID of master: " + str(gid_master))
-        print("Magic Number: " + str(magicNumberMaster))
-        print("My RID: " + str(slaveRID))
+        print("GID of master: " + str(gid_master) + "\n")
+        print("Magic Number: " + str(magicNumberMaster) + "\n")
+        print("My RID: " + str(slaveRID) + "\n")
         dottedDecimal = hex_to_ip_decimal(nextSlaveIP)
-        print("IP Address of Next Slave: " + str(dottedDecimal))
-
-
-
-
-    while(INPUT == 2):
-        ringId = input("please enter a ring id: \n")
-        msg = input("please enter your message here: \n")
-        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
-        #need to implement all these
-        message = struct.pack('>blbbbqb', gid_slave, magicNumber, ttl, destRID, srcRID, msg, checksum)
-        #sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-
-    while(INPUT == 3):
-        #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        exit();
-
+        print("IP Address of Next Slave: " + str(dottedDecimal) + "\n")
+        addAnotherSlave = input("Press 1 to add another slave, or 0 to exit. ")
 
 finally:
     print >>sys.stderr, 'closing socket'
